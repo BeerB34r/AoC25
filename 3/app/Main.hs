6ee708636cb390fa200477d407159277ac199bb0
Mats de Beer <matsdebeerk@gmail.com>
2025-12-04 01:32:55 +0100

    AoC day 3 part 2
    that was easier when i stopped and stepped back for half a moment

--- 3/app/Main.hs
+++ 3/app/Main.hs
@@ -1,17 +1,25 @@
 module Main where
 
 import Data.Char
-import Data.List
 
 main :: IO ()
 main = do
  print . sum . map maxJolt . lines =<< readFile "./example"
  print . sum . map maxJolt . lines =<< readFile "./puzzle"
- -- Naive bruteforce, the length of an individual bank is 100, so the
- -- subsequenceis a list of 100! numbers (yikes)
- print . sum . map (maximum . map read . filter (\x -> length x == 12) . subsequences :: String -> Integer) . lines =<< readFile "./example"
+ print . sum . map maxUJolt . lines =<< readFile "./example"
+ print . sum . map maxUJolt . lines =<< readFile "./puzzle"
 
 maxJolt :: [Char] -> Int
 maxJolt [] = -1
 maxJolt [x] = digitToInt x
 maxJolt (x:xs) = max (digitToInt x * 10 + digitToInt (maximum xs)) (maxJolt xs)
+
+maxUJolt :: [Char] -> Int
+maxUJolt [] = -1
+maxUJolt x = read $ foldl (incrementJolt) (take 12 x) (drop 12 x)
+
+incrementJolt :: [Char] -> Char -> [Char]
+incrementJolt x y = maximum $ (map (++ [y]) . removeAny $ x) ++ [x]
+
+removeAny :: [Char] -> [[Char]]
+removeAny x = map (\(a,b) -> a ++ drop 1 b) . zipWith (\a b -> splitAt a b) [0..] . take (length x) . cycle $ [x]
