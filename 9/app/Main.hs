dd093ba9470ead0a659f1856f32d1fff409c7fb6
Mats de Beer <mde-beer@student.codam.nl>
2025-12-16 19:37:26 +0100

    AoC day 9 part 1

--- 9/app/Main.hs
+++ 9/app/Main.hs
@@ -4,12 +4,21 @@ type Point = (Int,Int)
 
 main :: IO ()
 main = do
-    print $ rectArea (2,5) (9,7)
+    print . part1 =<< readFile "./example"
+    print . part1 =<< readFile "./puzzle"
     putStrLn "Hello, Haskell!"
 
+part1 :: String -> Int
+part1 s = maximum . map (`largestRectangle` points) $ points
+    where 
+        points = map ((\x -> (head x, x !! 1)) . map read . words . map (\x -> if x == ',' then ' ' else x)) . lines $ s
+
+largestRectangle :: Point -> [Point] -> Int
+largestRectangle _ [] = -1
+largestRectangle p (x:xs) = max (rectArea p x) (largestRectangle p xs)
+
 rectArea :: Point -> Point -> Int
-rectArea a b = do
-    let c = (fst a, snd b)
-    let l = abs (max(snd c,snd a) - min(snd c, snd a))
-    let w = abs (max(fst c,fst b) - min(fst c, fst b))
-    if l == 0 then if w == 0 then 0 else l * w else w
+rectArea a b = l * w
+    where
+        l = (+1) . abs $ max (snd b) (snd a) - min (snd b) (snd a) 
+        w = (+1) . abs $ max (fst a) (fst b) - min (fst a) (fst b)
